2d47ce2b8001b20b759a5fc38fddbd2920cae166
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-03-29 20:13:23 +0100

    Bump version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,7 +72,7 @@ android {
         applicationId = "de.rakhman.cooking"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
     }
     packaging {
