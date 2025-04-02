6c8f8e3f9229a3525d91e0f0f6383dd4777eee0b
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-04-02 19:37:21 +0200

    Implement opening sheet

--- composeApp/src/commonMain/kotlin/de/rakhman/cooking/ui/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/de/rakhman/cooking/ui/SettingsScreen.kt
@@ -1,6 +1,9 @@
 package de.rakhman.cooking.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
@@ -10,6 +13,7 @@ import androidx.compose.ui.unit.*
 import de.rakhman.cooking.events.SpreadsheetIdChangedEvent
 import de.rakhman.cooking.states.ScreenState
 import de.rakhman.cooking.states.SettingsState
+import de.rakhman.cooking.*
 import io.sellmair.evas.compose.EvasLaunching
 import io.sellmair.evas.compose.composeValue
 import io.sellmair.evas.emit
@@ -17,13 +21,13 @@ import io.sellmair.evas.set
 
 @Composable
 fun SettingsScreen(modifier: Modifier) {
-    Column(modifier = modifier.padding(12.dp, 4.dp, 12.dp, 12.dp)) {
+    Column(modifier = modifier.padding(12.dp, 4.dp, 12.dp, 12.dp).fillMaxSize()) {
         val settings = SettingsState.composeValue()
         var spreadSheetId by remember { mutableStateOf(settings?.spreadSheetsId ?: "") }
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth().padding(bottom = 12.dp),
             value = spreadSheetId,
-            onValueChange = { spreadSheetId = it },
+            onValueChange = { spreadSheetId = it.trim() },
             label = { Text("Google Sheets ID") },
             textStyle = LocalTextStyle.current.copy(fontSize = 18.sp),
             singleLine = true
@@ -31,15 +35,28 @@ fun SettingsScreen(modifier: Modifier) {
 
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = EvasLaunching { SpreadsheetIdChangedEvent(spreadSheetId).emit(); ScreenState.set(ScreenState.Plan) }
+            onClick = EvasLaunching { SpreadsheetIdChangedEvent(spreadSheetId.trim()).emit(); ScreenState.set(ScreenState.Plan) }
         ) {
             Text("Speichern")
         }
 
+        val context = getContext()
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick =  { openUrl("https://docs.google.com/spreadsheets/d/$spreadSheetId", context) },
+            colors = ButtonDefaults.filledTonalButtonColors(),
+            enabled = spreadSheetId.isNotBlank()
+        ) {
+            Text("Google Sheet öffnen")
+        }
+
+        Spacer(Modifier.weight(1f))
+
         Button(
             modifier = Modifier.fillMaxWidth(),
             onClick = EvasLaunching { ScreenState.set(ScreenState.Plan) },
-            colors = ButtonDefaults.outlinedButtonColors()
+            colors = ButtonDefaults.elevatedButtonColors()
         ) {
             Text("Schließen")
         }
