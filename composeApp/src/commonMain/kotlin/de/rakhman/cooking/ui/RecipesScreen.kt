73b524d3a0cf7acbd126a2c6ca3fb3a4ad800e67
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-04-25 15:43:45 +0200

    Implement sorting by counter

--- composeApp/src/commonMain/kotlin/de/rakhman/cooking/ui/RecipesScreen.kt
+++ composeApp/src/commonMain/kotlin/de/rakhman/cooking/ui/RecipesScreen.kt
@@ -8,12 +8,14 @@ import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.*
 import de.rakhman.cooking.Recipe
 import de.rakhman.cooking.states.RecipesState
 import de.rakhman.cooking.states.ScreenState
 import io.sellmair.evas.compose.composeValue
 import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
 import recipeplan.composeapp.generated.resources.*
 
 @Composable
@@ -22,7 +24,7 @@ fun RecipesScreen(modifier: Modifier) {
     Column(modifier = modifier) {
         when (recipeState) {
             is RecipesState.Success -> {
-                Recipes(recipeState.recipes.sortedBy { it.title.lowercase() })
+                Recipes(recipeState.recipes)
             }
 
             RecipesState.Loading -> {
@@ -34,29 +36,54 @@ fun RecipesScreen(modifier: Modifier) {
     }
 }
 
+enum class SortOrder(
+    val comparator: Comparator<Recipe>
+) {
+    Name(compareBy { it.title.lowercase() }),
+    Counter(compareByDescending<Recipe> { it.counter }.thenBy { it.title }),
+}
+
 @Composable
 private fun Recipes(recipes: List<Recipe>) {
     var filter by remember { mutableStateOf("") }
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth().padding(12.dp, 4.dp, 12.dp, 12.dp),
-        value = filter,
-        onValueChange = { filter = it },
-        label = { Text(stringResource(Res.string.search)) },
-        trailingIcon = {
-            if (filter.isNotEmpty()) {
-                IconButton(
-                    modifier = Modifier.padding(4.dp, 0.dp),
-                    onClick = { filter = "" },
-                ) {
-                    Icon(
-                        Icons.Default.Clear,
-                        contentDescription = stringResource(Res.string.clear_search),
-                    )
+    var sort by remember { mutableStateOf(SortOrder.Name) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(12.dp, 4.dp, 12.dp, 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.weight(1f).padding(end = 12.dp, bottom = 8.dp),
+            value = filter,
+            onValueChange = { filter = it },
+            label = { Text(stringResource(Res.string.search)) },
+            trailingIcon = {
+                if (filter.isNotEmpty()) {
+                    IconButton(
+                        modifier = Modifier.padding(4.dp, 0.dp),
+                        onClick = { filter = "" },
+                    ) {
+                        Icon(
+                            Icons.Default.Clear,
+                            contentDescription = stringResource(Res.string.clear_search),
+                        )
+                    }
                 }
-            }
-        },
-        textStyle = LocalTextStyle.current.copy(fontSize = 18.sp)
-    )
+            },
+            textStyle = LocalTextStyle.current.copy(fontSize = 18.sp)
+        )
+
+        IconButton(onClick = {
+            sort = SortOrder.entries[(sort.ordinal + 1) % SortOrder.entries.size]
+        }) {
+            Icon(
+                imageVector = vectorResource(Res.drawable.bootstrap_sort_down),
+                contentDescription = stringResource(Res.string.sort),
+                modifier = Modifier.size(24.dp)
+            )
+        }
+    }
+
     HorizontalDivider()
 
     if (recipes.isEmpty()) {
@@ -67,11 +94,11 @@ private fun Recipes(recipes: List<Recipe>) {
     }
 
     val listState = rememberLazyListState()
-    LaunchedEffect(filter) { listState.scrollToItem(0) }
+    LaunchedEffect(filter, sort) { listState.scrollToItem(0) }
 
     LazyColumn(state = listState, contentPadding = PaddingValues(bottom = 70.dp)) {
         val filteredList = if (filter.isEmpty()) {
-            recipes
+            recipes.sortedWith(sort.comparator)
         } else {
             recipes
                 .filter { it.matchesFilter(filter.trim()) }
