b8eeee2a51eb4959519824f8bd9f91f3da84c351
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-05-23 10:33:40 +0200

    Move RecipeDropdown to its own file

---
+++ composeApp/src/commonMain/kotlin/de/rakhman/cooking/ui/RecipeDropdown.kt
@@ -0,0 +1,109 @@
+package de.rakhman.cooking.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.unit.*
+import de.rakhman.cooking.Recipe
+import de.rakhman.cooking.events.*
+import de.rakhman.cooking.getContext
+import de.rakhman.cooking.shareRecipe
+import de.rakhman.cooking.shareToBring
+import de.rakhman.cooking.states.ID_TEMPORARY
+import de.rakhman.cooking.states.ScreenState
+import io.sellmair.evas.compose.EvasLaunching
+import io.sellmair.evas.emitAsync
+import io.sellmair.evas.set
+import org.jetbrains.compose.resources.getString
+import org.jetbrains.compose.resources.stringResource
+import recipeplan.composeapp.generated.resources.*
+
+@Composable
+fun RecipeDropdown(
+    recipe: Recipe,
+    screen: ScreenState.BaseScreen
+) {
+    var expanded by remember { mutableStateOf(false) }
+    val context = getContext()
+
+    Box(
+        modifier = Modifier.Companion.padding(end = 12.dp),
+        contentAlignment = Alignment.Companion.Center
+    ) {
+        if (recipe.id != ID_TEMPORARY) {
+            IconButton(onClick = { expanded = true }) {
+                Icon(imageVector = Icons.Filled.MoreVert, contentDescription = stringResource(Res.string.actions))
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                if (screen == ScreenState.Recipes) {
+                    DropdownMenuItem(
+                        text = { Text(stringResource(Res.string.add_to_shop)) },
+                        onClick = EvasLaunching {
+                            NotificationEvent(getString(Res.string.recipe_added_to_shop, recipe.title)).emitAsync()
+                            AddToShopEvent(recipe.id).emitAsync()
+                            expanded = false
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(stringResource(Res.string.add_to_plan)) },
+                        onClick = EvasLaunching {
+                            NotificationEvent(getString(Res.string.recipe_added_to_plan, recipe.title)).emitAsync()
+                            AddToPlanEvent(recipe.id).emitAsync()
+                            expanded = false
+                        }
+                    )
+                }
+                if (screen == ScreenState.Shop) {
+                    DropdownMenuItem(
+                        text = { Text(stringResource(Res.string.remove_from_shop)) },
+                        onClick = EvasLaunching {
+                            NotificationEvent(getString(Res.string.recipe_removed_from_shop, recipe.title)).emitAsync()
+                            RemoveFromShopEvent(recipe.id).emitAsync()
+                            expanded = false
+                        }
+                    )
+                }
+                DropdownMenuItem(
+                    text = { Text(stringResource(Res.string.edit)) },
+                    onClick = EvasLaunching {
+                        ScreenState.Key.set(
+                            ScreenState.Add(
+                                target = screen,
+                                editingRecipe = recipe
+                            )
+                        )
+                        expanded = false
+                    }
+                )
+                DropdownMenuItem(
+                    text = { Text(stringResource(Res.string.delete)) },
+                    onClick = EvasLaunching {
+                        DeleteRequestEvent(recipe).emitAsync()
+                        expanded = false
+                    }
+                )
+                DropdownMenuItem(
+                    text = { Text(stringResource(Res.string.share)) },
+                    onClick = { shareRecipe(recipe.title, recipe.url, context); expanded = false }
+                )
+                recipe.url?.let { url ->
+                    DropdownMenuItem(
+                        text = { Text(stringResource(Res.string.add_to_bring)) },
+                        onClick = { shareToBring(recipe.title, url, context); expanded = false }
+                    )
+                }
+            }
+        } else {
+            CircularProgressIndicator(modifier = Modifier.Companion.padding(12.dp).width(24.dp).height(24.dp))
+        }
+    }
+}
